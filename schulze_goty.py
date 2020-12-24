ceef98d255ed80c68787281ebbc87d4f05a9e431
Wok <woctezuma@users.noreply.github.com>
2020-12-24 15:39:57 +0100

    Fix call to steamspypi.load()

--- schulze_goty.py
+++ schulze_goty.py
@@ -345,7 +345,7 @@ def compute_steam_era_goty(ballot_year, ballot_filename=None):
 
     raw_votes = parse_votes(data)
 
-    steamspy_database = steamspypi.api.load()
+    steamspy_database = steamspypi.load()
     num_closest_neighbors = 3
 
     # The following parameter can only have an effect if it is strictly greater than 1.
